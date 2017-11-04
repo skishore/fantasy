d3dc265fb7bd3e5f816255dc9f5564964a0d5620
Shaunak Kishore <kshaunak@gmail.com>
2017-11-04 13:43:16 -0400

    Check in basic transliteration logic

---
+++ src/hindi/transliterator.ts
@@ -0,0 +1,38 @@
+import {assert} from '../lib/base';
+import {SYMBOLS, TRANSLITERATIONS, VOWELS, VOWEL_SIGNS} from './devanagari';
+
+const consonant = (ch: string): boolean => {
+  const code = ch.charCodeAt(0);
+  return ('क'.charCodeAt(0) <= code && code <= 'ह'.charCodeAt(0)) ||
+         ('क़'.charCodeAt(0) <= code && code <= 'य़'.charCodeAt(0));
+}
+
+const normalize = (ch: string): string => {
+  return VOWEL_SIGNS[ch] || ch;
+}
+
+const transliterate = (word: string): string => {
+  const result = [];
+  const characters = Array.from(word).map(normalize);
+  for (let i = 0; i < characters.length; i++) {
+    const ch = characters[i];
+    const transliteration = TRANSLITERATIONS[ch];
+    assert(transliteration != null, () => `Invalid Devanagari: ${ch}`);
+    result.push(transliteration);
+    const next = characters[i + 1];
+    if (!!next && consonant(ch) && consonant(next)) {
+      result.push('a');
+    }
+  }
+  return result.join('');
+}
+
+export {transliterate};
+
+const words = `
+इक्कीस
+बाइस
+`.trim().split('\n');
+for (const word of words) {
+  console.log(`${word} -> ${transliterate(word)}`);
+}
