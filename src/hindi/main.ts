a966ff03b65a2bc183ccd5f85907db79b3fd10ad
Shaunak Kishore <kshaunak@gmail.com>
2017-11-13 14:36:27 +0530

    Get ambiguous transliterations working

--- src/hindi/main.ts
+++ src/hindi/main.ts
@@ -4,10 +4,20 @@ import {transliterate} from './transliterator';
 declare const require: any;
 const fs = require('fs');
 
+const items: {keys: string[], value: string}[] = [];
 const data: string = fs.readFileSync('datasets/unicode.txt', 'utf-8');
-for (const line of data.split('\n').slice(0, 10)) {
+const lines = data.split('\n').slice(0, 30967);
+console.log(`Reading ${lines.length} words...`);
+for (const line of lines) {
   const [str, wx, word] = line.split(' ');
   if (!word || word.startsWith('#')) continue;
   const frequency = parseInt(str, 10);
-  console.log(`${word} -> ${transliterate(word)}`);
+  const transliterations = transliterate(word);
+  transliterations.forEach(
+      (x) => items.push({keys: Array.from(x), value: word}));
 }
+
+console.log(`Building trie with ${items.length} items...`);
+const trie = Trie.new(items);
+const deduped = Trie.serialize(trie).length;
+console.log(`Done! Compressed trie has ${deduped} distinct nodes.`);
--- src/hindi/transliterator.ts
+++ src/hindi/transliterator.ts
@@ -1,30 +1,40 @@
 import {assert} from '../lib/base';
 import {SYMBOLS, TRANSLITERATIONS, VOWELS, VOWEL_SIGNS} from './devanagari';
 
+const bindu = (ch: string): boolean =>
+    ch === SYMBOLS.anusvara || ch === SYMBOLS.chandrabindu;
+
 const consonant = (ch: string): boolean => {
   const code = ch.charCodeAt(0);
   return ('क'.charCodeAt(0) <= code && code <= 'ह'.charCodeAt(0)) ||
          ('क़'.charCodeAt(0) <= code && code <= 'य़'.charCodeAt(0));
 }
 
-const normalize = (ch: string): string => {
-  return VOWEL_SIGNS[ch] || ch;
-}
+const cross = (xs: string[], ys: string[]): string[] =>
+    [].concat.apply([], xs.map((x) => ys.map((y) => x + y)));
 
-const transliterate = (word: string): string => {
-  const result = [];
+const normalize = (ch: string): string => VOWEL_SIGNS[ch] || ch;
+
+const transliterate = (word: string): string[] => {
+  const result: string[][] = [];
   const characters = Array.from(word).map(normalize);
+  let last_was_consonant = false;
   for (let i = 0; i < characters.length; i++) {
     const ch = characters[i];
-    const transliteration = ch === ' ' ? ' ' : TRANSLITERATIONS[ch];
+    const transliteration = TRANSLITERATIONS[ch];
     assert(transliteration != null, () => `Invalid Devanagari: ${ch}`);
-    result.push(transliteration);
-    const next = characters[i + 1];
-    if (!!next && consonant(ch) && consonant(next)) {
-      result.push('a');
+    if (last_was_consonant && bindu(ch)) {
+      result.push(['a']);
+    } else if (last_was_consonant && consonant(ch)) {
+      result.push(['a', '']);
     }
+    last_was_consonant = consonant(ch);
+    result.push(transliteration);
   }
-  return result.join('');
+  if (last_was_consonant) {
+    result.push(['a', '']);
+  }
+  return result.reduce(cross, ['']);
 }
 
 export {transliterate};
