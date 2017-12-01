c3b3e801a1afd5f1b9697ce9302c696ec244953b
Shaunak Kishore <kshaunak@gmail.com>
2017-12-01 00:58:29 -0500

    Improve Trie library functionality

--- src/lib/trie.ts
+++ src/lib/trie.ts
@@ -1,86 +1,104 @@
-interface Item<K,V> {keys: K[], value: V};
-interface Memo<K,V> {dict: Map<string,number>, nodes: Trie<K,V>[]};
-interface Trie<K,V> {edges: Map<K,Trie<K,V>>, values: Set<V>};
+interface Data<K,V> {edges: (Map<K,number>|null)[], nodes: (Set<V>|null)[]};
+interface Memo<K,V> {data: Data<K,V>, dict: Map<string,number>};
 
-// WARNING: Calling add on an optimized Trie will cause severe correctness
-// issues because of shared nodes, so we do not export it from this file.
-const add = <K,V>(item: Item<K,V>, trie: Trie<K,V>) => {
-  if (item.keys.length === 0) {
-    trie.values.add(item.value);
-    return;
+type Item<K,V> = [Iterable<K>, V];
+type Serialized<K,V> = [([K, number][] | null)[], (V[] | null)[]];
+
+const add = <K,V>(data: Data<K,V>, i: number, keys: K[], value: V): number => {
+  if (keys.length === 0) {
+    const set = data.nodes[i];
+    if (set && set.has(value)) return i;
+    const result = share(data, i);
+    (data.nodes[result] = set ? new Set(set) : new Set()).add(value);
+    return result;
   }
-  const key = item.keys[0];
-  if (!trie.edges.has(key)) {
-    trie.edges.set(key, {edges: new Map(), values: new Set()});
-  }
-  add({keys: item.keys.slice(1), value: item.value}, trie.edges.get(key)!);
+  const map = data.edges[i];
+  const [head, tail] = [keys[0], keys.slice(1)];
+  const index = (map && map.get(head)) || 0;
+  const child = add(data, index, tail, value);
+  if (child === index) return i;
+  const result = share(data, i);
+  (data.edges[result] = map ? new Map(map) : new Map()).set(head, child);
+  return result;
 }
 
-const dedupe = <K,V>(trie: Trie<K,V>, memo: Memo<K,V>): number => {
-  const indices = Array.from(trie.edges.entries()).map(
-      ([key, child]): [K, number] => [key, dedupe(child, memo)]);
-  const id = JSON.stringify([indices.sort(), Array.from(trie.values).sort()]);
+const dedupe = <K,V>(data: Data<K,V>, i: number, memo: Memo<K,V>): number => {
+  const [map, set] = [data.edges[i], data.nodes[i]];
+  const edges = map && Array.from(map.entries()).map(
+      ([key, child]): [K, number] => [key, dedupe(data, child, memo)]).sort();
+  const nodes = set && Array.from(set).sort();
+  const id = JSON.stringify([edges, nodes]);
   if (!memo.dict.has(id)) {
-    const node: Trie<K,V> = {edges: new Map(), values: new Set(trie.values)};
-    indices.forEach(([key, i]) => node.edges.set(key, memo.nodes[i]));
-    memo.dict.set(id, memo.nodes.length);
-    memo.nodes.push(node);
+    memo.data.edges.push(edges && new Map(edges));
+    memo.data.nodes.push(nodes && new Set(nodes));
+    memo.dict.set(id, memo.data.edges.length - 1);
   }
   return memo.dict.get(id)!;
 }
 
-const items = <K,V>(trie: Trie<K,V>): Item<K,V>[] => {
-  const result: Item<K,V>[] = [];
-  trie.values.forEach((x) => result.push({keys: [], value: x}));
-  for (const [key, child] of trie.edges.entries()) {
-    for (const {keys, value} of items(child)) {
-      result.push({keys: [key].concat(keys), value});
+const empty = <K,V>(): Data<K,V> => ({edges: [null], nodes: [null]});
+
+const items = <K,V>(data: Data<K,V>, i: number): Item<K,V>[] => {
+  const [map, set] = [data.edges[i], data.nodes[i]];
+  const nodes = Array.from(set || new Set<V>()).sort();
+  const result = nodes.map((x): Item<K,V> => ([[], x]));
+  for (const [key, child] of Array.from(map || new Map<K,number>()).sort()) {
+    for (const [keys, value] of items(data, child)) {
+      result.push([[key].concat(Array.from(keys)), value]);
     }
   }
   return result;
 }
 
-const __new__ = <K,V>(items: Item<K,V>[]): Trie<K,V> => {
-  const result: Trie<K,V> = {edges: new Map(), values: new Set()};
-  items.forEach((x) => add(x, result));
-  return optimize(result);
+const share = <K,V>(data: Data<K,V>, i: number): number => {
+  data.edges.push(data.edges[i]);
+  data.nodes.push(data.nodes[i]);
+  return data.edges.length - 1;
 }
 
-const optimize = <K,V>(trie: Trie<K,V>): Trie<K,V> => {
-  const memo = {dict: new Map(), nodes: []};
-  return memo.nodes[dedupe(trie, memo)];
-}
+// The public interface of this file is an easy-to-use class.
 
-// JSON-compatible serialization for an existing trie.
-
-type Serialized<K,V> = [[K, number][], V[]];
-
-const deserialize = <K,V>(nodes: Serialized<K,V>[]): Trie<K,V> => {
-  const tries: Trie<K,V>[] = [];
-  for (const [adjacency, values] of nodes) {
-    const edges = adjacency.map(
-        ([key, index]): [K, Trie<K,V>] => [key, tries[index]]);
-    tries.push({edges: new Map(edges), values: new Set(values)});
+class Trie<K,V> {
+  private data: Data<K,V>;
+  constructor(items?: Item<K,V>[]) {
+    this.data = empty();
+    (items || []).forEach(([k, v]) => this.add(k, v));
+    this.compress();
   }
-  return tries.pop()!;
-}
-
-const serialize = <K,V>(trie: Trie<K,V>): Serialized<K,V>[] => {
-  const index = new Map<Trie<K,V>,number>();
-  const nodes: Serialized<K,V>[] = [];
-  const visit = (node: Trie<K,V>): number => {
-    if (!index.has(node)) {
-      const adjacency = Array.from(node.edges).map(
-          ([key, value]): [K, number] => [key, visit(value)]);
-      index.set(node, nodes.length);
-      nodes.push([adjacency, Array.from(node.values)]);
+  add(keys: Iterable<K>, value: V) {
+    const i = this.data.edges.length - 1;
+    add(this.data, i, Array.from(keys), value);
+  }
+  compress() {
+    const memo: Memo<K,V> = {data: empty(), dict: new Map()};
+    dedupe(this.data, this.data.edges.length - 1, memo);
+    this.data = memo.data;
+  }
+  entries(): Item<K,V>[] {
+    return items(this.data, this.data.edges.length - 1);
+  }
+  get(keys: Iterable<K>): V[] {
+    let current = this.data.edges.length - 1;
+    for (const key of keys) {
+      const edges = this.data.edges[current];
+      const next = edges && edges.get(key);
+      if (!next) return [];
+      current = next;
     }
-    return index.get(node)!;
+    const nodes = this.data.nodes[current];
+    return nodes ? Array.from(nodes).sort() : [];
   }
-  visit(trie);
-  return nodes;
+  serialize(): Serialized<K,V> {
+    const edges = this.data.edges.map((x) => x && Array.from(x));
+    const nodes = this.data.nodes.map((x) => x && Array.from(x));
+    return [edges, nodes];
+  }
+  static deserialize<K,V>(serialized: Serialized<K,V>): Trie<K,V> {
+    const trie = new Trie<K,V>();
+    trie.data.edges = serialized[0].map((x) => x && new Map(x));
+    trie.data.nodes = serialized[1].map((x) => x && new Set(x));
+    return trie;
+  }
 }
-
-const Trie = {deserialize, items, new: __new__, serialize};
 
 export {Trie};
--- test/lib/trie.ts
+++ test/lib/trie.ts
@@ -2,12 +2,15 @@ import {Option} from '../../src/lib/base';
 import {Trie} from '../../src/lib/trie';
 import {Test} from '../test';
 
+type Item<V> = [string[], V];
+
 const length = <K,V>(trie: Trie<K,V>): number => {
-  return Trie.serialize(trie).length;
+  // Subtract one to account for the initial dummy node in these tries.
+  return trie.serialize()[0].length - 1;
 }
 
 const serde = <K,V>(trie: Trie<K,V>): Trie<K,V> => {
-  const serialized = JSON.stringify(Trie.serialize(trie));
+  const serialized = JSON.stringify(trie.serialize());
   return Trie.deserialize(JSON.parse(serialized));
 }
 
@@ -17,38 +20,62 @@ const subsets = <T>(values: T[]): T[][] => {
   return child.concat(child.map((x) => [values[0]].concat(x)));
 }
 
-const values = <K,V>(trie: Trie<K,V>, keys: K[]): Option<V[]> => {
-  if (keys.length === 0) return {some: Array.from(trie.values)};
-  const node = trie.edges.get(keys[0]);
-  return node ? values(node, keys.slice(1)) : null;
-}
-
 const trie: Test = {
-  trie_includes_all_keys: () => {
+  all_entries_included: () => {
     const keys = subsets(Array.from('abcde'));
-    const trie = Trie.new(keys.map((x) => ({keys: x, value: true})));
-    keys.forEach((x) => Test.assert_eq(values(trie, x), {some: [true]}));
-    Test.assert_eq(values(trie, ['b', 'a']), null);
+    const trie = new Trie(keys.map((x): Item<boolean> => ([x, true])));
+    keys.forEach((x) => Test.assert_eq(trie.get(x), [true]));
+    Test.assert_eq(trie.entries().length, 32);
+    Test.assert_eq(trie.get('ba'), []);
   },
-  trie_serialization_works: () => {
+  all_entries_preserved_by_serialize: () => {
     const keys = subsets(Array.from('abcde'));
-    const trie = serde(Trie.new(keys.map((x) => ({keys: x, value: true}))));
-    keys.forEach((x) => Test.assert_eq(values(trie, x), {some: [true]}));
-    Test.assert_eq(values(trie, ['b', 'a']), null);
+    const trie = new Trie(keys.map((x): Item<boolean> => ([x, true])));
+    keys.forEach((x) => Test.assert_eq(trie.get(x), [true]));
+    Test.assert_eq(trie.entries().length, 32);
+    Test.assert_eq(trie.get('ba'), []);
   },
-  trie_optimization_works: () => {
+  compression_yields_fewer_nodes: () => {
     const keys = subsets(Array.from('abcde'));
-    const base = Trie.new(keys.map((x) => ({keys: x, value: true})));
+    const base = new Trie(keys.map((x): Item<boolean> => ([x, true])));
     const trie = serde(base);
     Test.assert_eq(length(base), 6);
     Test.assert_eq(length(trie), 6);
   },
-  trie_optimization_handles_varied_keys: () => {
+  compression_handles_varied_keys: () => {
     const keys = subsets(Array.from('abcde'));
-    const base = Trie.new(keys.map((x) => ({keys: x, value: x.length % 2})));
+    const base = new Trie(keys.map((x): Item<number> => ([x, x.length % 2])));
     const trie = serde(base);
     Test.assert_eq(length(base), 10);
     Test.assert_eq(length(trie), 10);
+  },
+  dynamic_updates_work: () => {
+    const old_keys = subsets(Array.from('abcde'));
+    const new_keys = subsets(Array.from('abcd'));
+    const trie = new Trie(old_keys.map((x): Item<boolean> => ([x, true])));
+    new_keys.forEach((x) => Test.assert_eq(trie.get(x.concat('e')), [true]));
+    new_keys.forEach((x) => Test.assert_eq(trie.get(x), [true]));
+    Test.assert_eq(trie.entries().length, 32);
+    Test.assert_eq(length(trie), 6);
+
+    new_keys.map((x) => trie.add(x, false));
+    new_keys.forEach((x) => Test.assert_eq(trie.get(x.concat('e')), [true]));
+    new_keys.forEach((x) => Test.assert_eq(trie.get(x), [false, true]));
+    Test.assert_eq(trie.entries().length, 48);
+    Test.assert_eq(length(trie), 54);
+
+    trie.compress();
+    new_keys.forEach((x) => Test.assert_eq(trie.get(x.concat('e')), [true]));
+    new_keys.forEach((x) => Test.assert_eq(trie.get(x), [false, true]));
+    Test.assert_eq(trie.entries().length, 48);
+    Test.assert_eq(length(trie), 6);
+  },
+  read_methods_work: () => {
+    const keys = subsets(Array.from('abcde'));
+    const trie = new Trie(keys.map((x): Item<number> => ([x, x.length % 2])));
+    const entries = keys.sort().map((x) => [x, x.length % 2]);
+    Test.assert_eq<any>(entries, trie.entries());
+    Test.assert_eq(trie.entries().length, 32);
   },
 };
 
