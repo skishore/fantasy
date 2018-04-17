7cbb85656256046512baf6639badf10ecbf91fb2
Shaunak Kishore <kshaunak@gmail.com>
2018-04-17 04:25:52 -0400

    [lex] Remove deprecated grammar files

--- test/main.ts
+++ test/main.ts
@@ -5,18 +5,10 @@ import {wx} from './hindi/wx';
 import {lexer} from './lib/lexer';
 import {template} from './lib/template';
 import {trie} from './lib/trie';
-import {compiler} from './parsing/compiler';
-import {corrector} from './parsing/corrector';
-import {parser} from './parsing/parser';
-import {generator} from './parsing/generator';
 import {Test} from './test';
 
 const kTestCases = {
-  compiler,
-  corrector,
-  generator,
   lexer,
-  parser,
   template,
   transliterator,
   trie,
