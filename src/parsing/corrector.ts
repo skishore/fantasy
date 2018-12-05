e0e66656a57bc6b6b9610798854734b0f7508d53
Shaunak Kishore <kshaunak@gmail.com>
2018-12-05 01:24:56 -0500

    Track correction issues correctly

--- src/parsing/corrector.ts
+++ src/parsing/corrector.ts
@@ -1,45 +1,43 @@
 import {Option, assert, range} from '../lib/base';
 import {Lambda, Template} from '../template/lambda';
-import {Tense, Tree, XGrammar, XMatch} from './extensions';
+import {Tense, Tree, XGrammar, XMatch, XRule} from './extensions';
 import {Generator} from './generator';
 
 // Internal implementation details of the correction algorithm.
-//
-// TODO(skishore): The way we track issues in the State type is broken.
-// They're stored in a map from node to list of issues, but the set of nodes
-// returned by the algorithm is completely disjoint from the inputs. Solving
-// this problem is nontrivial because the correction can create and then
-// discard entire subtrees, and because whenever we touch a child, we update
-// all of its parents as well.
 
-interface Issue {
-  actual: string;
-  category: string;
-  expected: string;
+interface Mapping<T> {
+  errors: string[];
+  new_node: Tree<T>;
+  old_node: Tree<T>;
 }
 
 interface State<T> {
   grammar: XGrammar<T>;
-  issues: Map<Tree<T>, Issue[]>;
+  mapping: Mapping<T>[];
   tense: Tense;
 }
 
-const apply = <T>(actual: Tense[], state: State<T>): Issue[] => {
+const apply = <T>(actual: Tense[], state: State<T>): string[] => {
   if (actual.length === 0) return [];
-  const issues = actual.map(x => check(x, state.tense));
-  const okay = actual.filter((x, i) => issues[i].length === 0);
-  if (okay.length === 0) return issues.sort((a, b) => a.length - b.length)[0];
-  Object.assign(state.tense, merge(okay));
+  const errors = actual.map(x => check_tense(x, state.tense));
+  const okay = actual.filter((x, i) => errors[i].length === 0);
+  if (okay.length === 0) return errors.sort((a, b) => a.length - b.length)[0];
+  Object.assign(state.tense, merge_tense(okay));
   return [];
 };
 
-const check = (actual: Tense, expected: Tense): Issue[] => {
-  return Object.keys(expected)
-    .filter(x => actual[x] !== expected[x])
-    .map(x => ({actual: actual[x], category: x, expected: expected[x]}));
+const check_rules = <T>(rule: XRule<T>, tense: Tense): string[] => {
+  if (rule.split.score === -Infinity) return ['Invalid phrasing.'];
+  return check_tense(rule.data.tense, tense);
 };
 
-const merge = (actual: Tense[]): Tense => {
+const check_tense = (actual: Tense, expected: Tense): string[] => {
+  return Object.keys(expected)
+    .filter(x => actual[x] !== expected[x])
+    .map(x => `${x} should be ${expected[x]} (was: ${actual[x]}).`);
+};
+
+const merge_tense = (actual: Tense[]): Tense => {
   const first = actual[0] || {};
   if (actual.length <= 1) return first;
   const result: Tense = {};
@@ -51,52 +49,57 @@ const merge = (actual: Tense[]): Tense => {
 
 // The core recursive correction algorithm, which operates on a mutable state.
 
-const recurse = <T>(derivation: Tree<T>, state: State<T>): Tree<T> => {
+const recurse = <T>(old_node: Tree<T>, state: State<T>): Tree<T> => {
   // Correct leaf nodes using the provided term correction method.
-  if (derivation.type === 'leaf') {
-    const match = derivation.match;
-    const issues = apply(derivation.match.data.tenses, state);
-    if (issues.length === 0) return derivation;
-    // TODO(skishore): Record the issues in the state.
-    const maybe = state.grammar.lexer.fix(match, state.tense);
-    if (!maybe) return derivation;
-    assert(apply(maybe.data.tenses, state).length === 0);
-    return {...derivation, match: maybe};
+  if (old_node.type === 'leaf') {
+    let new_node = old_node;
+    const errors = apply(old_node.match.data.tenses, state);
+    if (errors.length > 0) {
+      const maybe = state.grammar.lexer.fix(old_node.match, state.tense);
+      if (maybe) new_node = {...old_node, match: maybe};
+      if (maybe) assert(apply(maybe.data.tenses, state).length === 0);
+    }
+    state.mapping.push({errors, new_node, old_node});
+    return new_node;
   }
 
-  let modified: Tree<T> = {...derivation};
-  modified.children = modified.children.slice();
+  let new_node: Tree<T> = {...old_node};
+  new_node.children = new_node.children.slice();
 
-  // Correct top-level issues at a derivation rule node by regenerating the
-  // entire subtree rooted at this node.
-  const issues = check(derivation.rule.data.tense, state.tense);
-  if (issues.length > 0) {
-    // TODO(skishore): Record the issues in the state.
-    const lhs = derivation.rule.lhs;
+  // Correct rule errors (where the rule's syntax is incompatible with the
+  // current tense state) by regenerating the entire tree rooted at this node.
+  //
+  // TODO(skishore): We should reuse subtrees that appear in both trees.
+  // For example, if a '$noun rang do' is an invalid rule but '$noun ka rang'
+  // is a valid one, we should reuse the $noun and rang subtrees.
+  const errors = check_rules(old_node.rule, state.tense);
+  if (errors.length > 0) {
+    const lhs = old_node.rule.lhs;
     const rules = state.grammar.rules.filter(
-      x => x.lhs === lhs && check(x.data.tense, state.tense).length === 0,
+      x => x.lhs === lhs && check_rules(x, state.tense).length === 0,
     );
-    const value = {some: derivation.value};
-    const fixed = Generator.generate_from_rules(state.grammar, rules, value);
-    if (fixed) modified = fixed.some;
+    const value = {some: old_node.value};
+    const maybe = Generator.generate_from_rules(state.grammar, rules, value);
+    if (maybe) new_node = maybe.some;
   }
-  if (modified.type !== 'node') throw Error('Invalid node replacement!');
-  apply([derivation.rule.data.tense], state);
+  if (new_node.type !== 'node') throw Error('Invalid node replacement!');
+  apply([new_node.rule.data.tense], state);
 
-  // Correct tense issues in each of the rule node's children.
-  const rule = modified.rule;
+  // Correct tense errors in each of the rule node's children.
+  const rule = new_node.rule;
   const checked = range(rule.rhs.length).map(() => false);
-  for (const i of modified.rule.data.precedence) {
-    modified.children[i] = recurse(modified.children[i], state);
+  for (const i of rule.data.precedence) {
+    new_node.children[i] = recurse(new_node.children[i], state);
     checked[i] = true;
   }
   const original = state.tense;
   for (let i = 0; i < checked.length; i++) {
     if (checked[i]) continue;
     const child_state = {...state, tense: {}};
-    modified.children[i] = recurse(modified.children[i], child_state);
+    new_node.children[i] = recurse(new_node.children[i], child_state);
   }
-  return modified;
+  state.mapping.push({errors, new_node, old_node});
+  return new_node;
 };
 
 // TODO(skishore): We need to wrap up this algorithm in a need interface that:
@@ -104,4 +107,7 @@ const recurse = <T>(derivation: Tree<T>, state: State<T>): Tree<T> => {
 //  1. Takes the "correct" method for correcting terms as input.
 //  2. Builds the state and calls the recursive algorithm above.
 //  3. Computes a diff between the resulting parse tree and the original one.
-//  4. For each diff chunk, records a list of issues causing that diff.
+//  4. Take a seed as input and derandomize this algorithm.
+//
+// TODO(skishore): Write a test for this code.
+// TODO(skishore): Write a parser from sexp -> XGrammar<T>.
