48caf75f6dfc12ec24be4f29504d1ff8302e16be
Shaunak Kishore <kshaunak@gmail.com>
2019-01-09 04:43:13 -0500

    Implement a real optimization

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ use std::collections::BTreeSet;
 use std::rc::Rc;
 use test::Bencher;
 
-type Failure = (Vec<Rc<String>>, usize);
+struct Failure {
+  expected: Vec<Rc<String>>,
+  remainder: usize,
+  total: usize,
+}
 
-type Output<T> = Option<(T, usize)>;
+type Output<'a, T> = Option<(T, &'a [u8])>;
 
-type Method<'a, T> = Fn(&'a [u8], usize, &mut Failure) -> Output<T> + 'a;
+type Method<'a, T> = Fn(&'a [u8], &mut Failure) -> Output<'a, T> + 'a;
 
 pub struct Parser<'a, T> {
   method: Box<Method<'a, T>>,
@@ -20,49 +24,50 @@ pub struct Parser<'a, T> {
 impl<'a, T> Parser<'a, T> {
   pub fn new<M>(method: M) -> Parser<'a, T>
   where
-    M: Fn(&'a [u8], usize, &mut Failure) -> Output<T> + 'a,
+    M: Fn(&'a [u8], &mut Failure) -> Output<'a, T> + 'a,
   {
     Parser { method: Box::new(method) }
   }
 
   pub fn parse(&self, x: &'a str) -> Result<T, String> {
-    let mut failure = (vec![], 0);
-    match (self.method)(x.as_bytes(), 0, &mut failure) {
-      Some((value, i)) => {
-        if i < x.len() {
-          Result::Err(format(&failure, i))
+    let total = x.len();
+    let mut failure = Failure { expected: vec![], remainder: total, total };
+    match (self.method)(x.as_bytes(), &mut failure) {
+      Some((value, remainder)) => {
+        if remainder.len() > 0 {
+          Result::Err(format(&failure, remainder.len()))
         } else {
           Ok(value)
         }
       }
-      None => Result::Err(format(&failure, 0)),
+      None => Result::Err(format(&failure, total)),
     }
   }
 }
 
-fn format(failure: &Failure, i: usize) -> String {
-  if failure.1 < i {
-    return format!("At index {}: expected: EOF", i);
+fn format(failure: &Failure, remainder: usize) -> String {
+  if failure.remainder > remainder {
+    return format!("At index {}: expected: EOF", failure.total - remainder);
   }
-  let expected: BTreeSet<_> = failure.0.iter().map(|x| x.to_string()).collect();
+  let expected: BTreeSet<_> = failure.expected.iter().map(|x| x.to_string()).collect();
   let expected: Vec<_> = expected.into_iter().collect();
-  format!("At index {}: expected: {}", failure.1, expected.join(" | "))
+  format!("At index {}: expected: {}", failure.total - failure.remainder, expected.join(" | "))
 }
 
-fn update(expected: Rc<String>, i: usize, failure: &mut Failure) {
-  if i < failure.1 {
+fn update(expected: Rc<String>, remainder: usize, failure: &mut Failure) {
+  if failure.remainder < remainder {
     return;
-  } else if i > failure.1 {
-    failure.0.clear();
-    failure.1 = i;
+  } else if failure.remainder > remainder {
+    failure.expected.clear();
+    failure.remainder = remainder;
   }
-  failure.0.push(expected);
+  failure.expected.push(expected);
 }
 
 fn any<'a, A: 'a>(parsers: Vec<Parser<'a, A>>) -> Parser<'a, A> {
-  Parser::new(move |x, i, f| {
+  Parser::new(move |x, f| {
     for parser in &parsers {
-      match (parser.method)(x, i, f) {
+      match (parser.method)(x, f) {
         Some(x) => return Some(x),
         None => continue,
       }
@@ -75,20 +80,20 @@ fn map<'a, A: 'a, B: 'a, F>(parser: Parser<'a, A>, callback: F) -> Parser<'a, B>
 where
   F: Fn(A) -> B + 'a,
 {
-  Parser::new(move |x, i, f| match (parser.method)(x, i, f) {
-    Some((value, i)) => Some((callback(value), i)),
+  Parser::new(move |x, f| match (parser.method)(x, f) {
+    Some((value, x)) => Some((callback(value), x)),
     None => None,
   })
 }
 
 fn mul<'a, A: 'a>(parser: Parser<'a, A>, min: usize) -> Parser<'a, Vec<A>> {
-  Parser::new(move |x, i, f| {
-    let mut position = i;
+  Parser::new(move |x, f| {
+    let mut remainder = x;
     let mut result = vec![];
     loop {
-      match (parser.method)(x, position, f) {
-        Some((value, i)) => {
-          position = i;
+      match (parser.method)(remainder, f) {
+        Some((value, x)) => {
+          remainder = x;
           result.push(value);
         }
         None => break,
@@ -97,31 +102,31 @@ fn mul<'a, A: 'a>(parser: Parser<'a, A>, min: usize) -> Parser<'a, Vec<A>> {
     if result.len() < min {
       None
     } else {
-      Some((result, position))
+      Some((result, remainder))
     }
   })
 }
 
 fn opt<'a, A: 'a>(a: Parser<'a, A>) -> Parser<'a, Option<A>> {
-  Parser::new(move |x, i, f| match (a.method)(x, i, f) {
-    Some((value, i)) => Some((Some(value), i)),
-    None => Some((None, i)),
+  Parser::new(move |x, f| match (a.method)(x, f) {
+    Some((value, x)) => Some((Some(value), x)),
+    None => Some((None, x)),
   })
 }
 
 fn seq<'a, A: 'a, B: 'a>(a: Parser<'a, A>, b: Parser<'a, B>) -> Parser<'a, (A, B)> {
-  Parser::new(move |x, i, f| {
-    (a.method)(x, i, f).map(|(a, i)| (b.method)(x, i, f).map(|(b, i)| ((a, b), i))).unwrap_or(None)
+  Parser::new(move |x, f| {
+    (a.method)(x, f).map(|(a, x)| (b.method)(x, f).map(|(b, x)| ((a, b), x))).unwrap_or(None)
   })
 }
 
 fn tag<'a>(tag: &'a str) -> Parser<'a, &'a str> {
   let expected = Rc::new(format!("{:?}", tag));
-  Parser::new(move |x, i, f| {
-    if x.iter().skip(i).take(tag.len()).eq(tag.as_bytes().iter()) {
-      Some((tag, i + tag.len()))
+  Parser::new(move |x, f| {
+    if x.iter().take(tag.len()).eq(tag.as_bytes().iter()) {
+      Some((tag, &x[tag.len()..]))
     } else {
-      update(expected.clone(), i, f);
+      update(expected.clone(), x.len(), f);
       None
     }
   })
@@ -132,11 +137,11 @@ where
   F: Fn(u8) -> bool + 'a,
 {
   let expected = Rc::new(format!("{:?}", name));
-  Parser::new(move |x, i, f| {
-    if i < x.len() && callback(x[i]) {
-      Some((x[i], i + 1))
+  Parser::new(move |x, f| {
+    if x.len() > 0 && callback(x[0]) {
+      Some((x[0], &x[1..]))
     } else {
-      update(expected.clone(), i, f);
+      update(expected.clone(), x.len(), f);
       None
     }
   })
