e476137acd3030b7884e445a22c3fb68a8e21a78
Shaunak Kishore <kshaunak@gmail.com>
2019-01-25 01:05:15 -0500

    Cut down on Candidate and State space usage

--- src/parser.rs
+++ src/parser.rs
@@ -45,21 +45,14 @@ pub enum Term {
 // A State is a rule accompanied with a "cursor" and a "start", where the
 // cursor is the position in the rule up to which we have a match and the
 // start is the token from which this match started.
-//
-// TODO: These structures use more memory than necessary. Next costs 16
-// bytes instead of 8 because of alignment issues. It can be replaced with
-// a void pointer instead. Candidate can be merged with Candidates (as in,
-// turned into a linked-list) and added directly to state as a pointer
-// rather than an Option. Finally, we can make cursor and start u16 and
-// thus get both the Candidate list and State down to 24 bytes each.
 
 #[derive(Clone)]
 struct Candidate<'a, T: Clone> {
-  next: Next<'a, T>,
+  down: *const u8,
+  next: *const Candidate<'a, T>,
   prev: *const State<'a, T>,
 }
 
-#[derive(Clone)]
 enum Next<'a, T: Clone> {
   Leaf(&'a Match<T>),
   Node(*const State<'a, T>),
@@ -67,36 +60,54 @@ enum Next<'a, T: Clone> {
 
 #[derive(Clone)]
 struct State<'a, T: Clone> {
-  candidate: Option<Candidate<'a, T>>,
-  cursor: usize,
+  candidate: *const Candidate<'a, T>,
+  cursor: u16,
   rule: &'a IndexedRule<'a, T>,
   score: f32,
-  start: usize,
+  start: u16,
 }
 
 impl<'a, T: Clone> Copy for Candidate<'a, T> {}
-impl<'a, T: Clone> Copy for Next<'a, T> {}
 impl<'a, T: Clone> Copy for State<'a, T> {}
 
 impl<'a, T: Clone> State<'a, T> {
   fn new(cursor: usize, rule: &'a IndexedRule<'a, T>, start: usize) -> Self {
-    Self { candidate: None, cursor, rule, score: std::f32::NEG_INFINITY, start }
+    let max = u16::max_value() as usize;
+    assert!(cursor <= max && start <= max);
+    let (cursor, start) = (cursor as u16, start as u16);
+    Self { candidate: std::ptr::null(), cursor, rule, score: std::f32::NEG_INFINITY, start }
+  }
+
+  fn cursor(&self) -> usize {
+    self.cursor as usize
+  }
+
+  fn down(&self, down: *const u8) -> Next<'a, T> {
+    assert!(self.cursor > 0);
+    match self.rule.rhs[self.cursor() - 1] {
+      Term::Symbol(_) => Next::Node(down as *const Self),
+      Term::Terminal(_) => Next::Leaf(unsafe { &*(down as *const Match<T>) }),
+    }
   }
 
   fn evaluate(&self) -> T {
-    assert!(self.cursor == self.rule.rhs.len());
-    let mut xs = Vec::with_capacity(self.cursor);
+    assert!(self.cursor() == self.rule.rhs.len());
+    let mut xs = Vec::with_capacity(self.cursor());
     let mut current = self;
     for _ in 0..self.cursor {
-      let candidate = current.candidate.as_ref().unwrap();
-      xs.push(match &candidate.next {
+      let Candidate { down, prev, .. } = unsafe { *current.candidate };
+      xs.push(match current.down(down) {
         Next::Leaf(x) => x.value.clone(),
-        Next::Node(x) => unsafe { (**x).evaluate() },
+        Next::Node(x) => unsafe { (*x).evaluate() },
       });
-      current = unsafe { &*candidate.prev };
+      current = unsafe { &*prev };
     }
     xs.reverse();
     (self.rule.callback)(&xs)
+  }
+
+  fn start(&self) -> usize {
+    self.start as usize
   }
 }
 
@@ -130,14 +141,9 @@ impl<'a, T: Clone> State<'a, T> {
 //
 //  states: A list of states ending here.
 
-struct Candidates<'a, T: Clone> {
-  candidate: Candidate<'a, T>,
-  next: *const Candidates<'a, T>,
-}
-
 struct Chart<'a, T: Clone> {
   grammar: &'a IndexedGrammar<'a, T>,
-  candidates: Arena<Candidates<'a, T>>,
+  candidates: Arena<Candidate<'a, T>>,
   completed: Vec<*const State<'a, T>>,
   scannable: Vec<*const State<'a, T>>,
   states: Arena<State<'a, T>>,
@@ -145,7 +151,7 @@ struct Chart<'a, T: Clone> {
 }
 
 struct Column<'a, T: Clone> {
-  candidates: FxHashMap<usize, *const Candidates<'a, T>>,
+  candidates: FxHashMap<usize, *const State<'a, T>>,
   completed: Vec<*const State<'a, T>>,
   scannable: Vec<*const State<'a, T>>,
   states: Vec<*const State<'a, T>>,
@@ -171,11 +177,7 @@ impl<'a, T: Clone> Chart<'a, T> {
     best_state.map(|x| x.evaluate())
   }
 
-  fn score(
-    &self,
-    candidates: &FxHashMap<usize, *const Candidates<'a, T>>,
-    state: *const State<'a, T>,
-  ) -> f32 {
+  fn score(&self, state: *const State<'a, T>) -> f32 {
     let state = unsafe { &mut *(state as *mut State<'a, T>) };
     if state.score > std::f32::NEG_INFINITY {
       return state.score;
@@ -183,24 +185,23 @@ impl<'a, T: Clone> Chart<'a, T> {
       state.score = state.rule.score;
       return state.score;
     }
-    let mut best_candidate = None;
+    let mut best_candidate = std::ptr::null();
     let mut best_score = std::f32::NEG_INFINITY;
-    let index = state.start * self.grammar.max_index + state.rule.index + state.cursor;
-    let mut node = candidates.get(&index).cloned().unwrap_or(std::ptr::null());
-    while !node.is_null() {
-      let Candidates { candidate, next: next_node } = unsafe { &*node };
-      node = *next_node;
-      let next_score = match candidate.next {
+    let mut candidate = state.candidate;
+    while !candidate.is_null() {
+      let Candidate { down, next, prev } = unsafe { *candidate };
+      let next_score = match state.down(down) {
         Next::Leaf(x) => x.score,
-        Next::Node(x) => self.score(candidates, x),
+        Next::Node(x) => self.score(x),
       };
-      let prev_score = self.score(candidates, candidate.prev) + next_score;
-      if prev_score > best_score {
-        best_candidate = Some(*candidate);
-        best_score = prev_score;
+      let score = self.score(prev) + next_score;
+      if score > best_score {
+        best_candidate = candidate;
+        best_score = score;
       }
+      candidate = next;
     }
-    assert!(best_candidate.is_some());
+    assert!(!best_candidate.is_null());
     assert!(best_score > std::f32::NEG_INFINITY);
     state.candidate = best_candidate;
     state.score = best_score;
@@ -209,14 +210,19 @@ impl<'a, T: Clone> Chart<'a, T> {
 
   fn step(&mut self, next: Next<'a, T>, state: *const State<'a, T>, column: &mut Column<'a, T>) {
     let state = unsafe { &*state };
-    let offset = state.start * self.grammar.max_index + state.rule.index + state.cursor + 1;
-    let values = column.candidates.entry(offset).or_insert(std::ptr::null());
-    if values.is_null() {
-      let state = State::new(state.cursor + 1, state.rule, state.start);
-      column.states.push(self.states.alloc(state));
+    let index = state.start() * self.grammar.max_index + state.rule.index + state.cursor() + 1;
+    let entry = column.candidates.entry(index).or_insert(std::ptr::null());
+    if entry.is_null() {
+      *entry = self.states.alloc(State::new(state.cursor() + 1, state.rule, state.start()));
+      column.states.push(*entry);
     }
-    let candidate = Candidate { next, prev: state };
-    *values = self.candidates.alloc(Candidates { candidate, next: *values });
+    let down = match next {
+      Next::Leaf(x) => x as *const Match<T> as *const u8,
+      Next::Node(x) => x as *const u8,
+    };
+    let next = unsafe { (**entry).candidate };
+    let candidate = self.candidates.alloc(Candidate { down, next, prev: state });
+    unsafe { (*(*entry as *mut State<T>)).candidate = candidate };
   }
 
   fn update(&mut self, index: usize, token: Option<&'a Token<T>>) {
@@ -236,7 +242,7 @@ impl<'a, T: Clone> Chart<'a, T> {
     } else if let Some(token) = token {
       scannable.iter().for_each(|x| {
         let state = unsafe { &**x };
-        if let Term::Terminal(t) = &state.rule.rhs[state.cursor] {
+        if let Term::Terminal(t) = &state.rule.rhs[state.cursor()] {
           if let Some(m) = token.matches.get(t) {
             self.step(Next::Leaf(m), state, &mut column);
           }
@@ -248,22 +254,22 @@ impl<'a, T: Clone> Chart<'a, T> {
     while i < column.states.len() {
       let state = unsafe { &*column.states[i] };
       i += 1;
-      if state.cursor == state.rule.rhs.len() {
-        let j = state.start * self.grammar.max_index + state.rule.lhs.0;
+      if state.cursor() == state.rule.rhs.len() {
+        let j = state.start() * self.grammar.max_index + state.rule.lhs.0;
         if let Some(wanted) = &wanted.get(&j) {
           wanted.iter().for_each(|x| self.step(Next::Node(state), *x, &mut column));
         }
-        if state.start == 0 {
+        if state.start() == 0 {
           column.completed.push(state);
         }
-        if state.start == index {
+        if state.start() == index {
           let entry = nullable.entry(state.rule.lhs.0).or_insert(state);
           if entry.rule.score < state.rule.score {
             *entry = state;
           }
         }
       } else {
-        match state.rule.rhs[state.cursor] {
+        match state.rule.rhs[state.cursor()] {
           Term::Symbol(lhs) => {
             if let Some(nullable) = nullable.get(&lhs.0) {
               self.step(Next::Node(*nullable), state, &mut column);
@@ -283,7 +289,7 @@ impl<'a, T: Clone> Chart<'a, T> {
     }
 
     column.states.iter().for_each(|x| {
-      self.score(&column.candidates, *x);
+      self.score(*x);
     });
     std::mem::swap(&mut column.completed, &mut self.completed);
     std::mem::swap(&mut column.scannable, &mut self.scannable);
