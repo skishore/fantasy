7224083b09c6544fb393ec457036444f7dccaad1
Shaunak Kishore <kshaunak@gmail.com>
2019-04-28 15:58:48 -0400

    Compute diffs while doing correction

--- src/nlu/corrector.rs
+++ src/nlu/corrector.rs
@@ -17,16 +17,10 @@ type Grammar<T> = super::base::Grammar<Option<T>, T>;
 type Lexer<T> = super::base::Lexer<Option<T>, T>;
 type Rule<T> = super::base::Rule<Option<T>, T>;
 
-struct Mapping<'a, T> {
-  errors: Vec<String>,
-  old_node: Child<'a, T>,
-  new_node: Child<'a, T>,
-}
-
 struct State<'a, 'b, T: Payload> {
+  diff: Vec<Diff<T>>,
   generator: &'b Generator<'a, T>,
   grammar: &'a Grammar<T>,
-  mapping: Vec<Mapping<'a, T>>,
   rng: &'b mut Rng,
   tense: Tense,
 }
@@ -35,6 +29,11 @@ struct State<'a, 'b, T: Payload> {
 
 impl<'a, 'b, T: Payload> State<'a, 'b, T> {
   // The subtree-rebuilding logic. TODO(skishore): Reuse as much as possible.
+
+  fn clone_tree(tree: &Derivation<'a, T>) -> Derivation<'a, T> {
+    let Derivation { children, rule, value } = tree;
+    Derivation { children: children.clone(), rule, value: value.clone() }
+  }
 
   fn check_rules(&self, rule: &Rule<T>) -> Vec<String> {
     let ok = rule.split.score != std::f32::NEG_INFINITY;
@@ -67,16 +66,19 @@ impl<'a, 'b, T: Payload> State<'a, 'b, T> {
   }
 
   fn see_leaf(&mut self, old: Rc<Match<T>>) -> Rc<Match<T>> {
-    let mut new = old.clone();
     let errors = self.tense.union_checked(&old.tenses);
-    if !errors.is_empty() {
-      let options = self.grammar.lexer.fix(&*old, &self.tense);
-      if !options.is_empty() {
-        new = options[self.rng.gen::<usize>() % options.len()].clone();
-        assert!(self.tense.union_checked(&new.tenses).is_empty());
-      }
+    if errors.is_empty() {
+      self.diff.push(Diff::Right(old.clone()));
+      return old.clone();
     }
-    self.mapping.push(Mapping { errors, old_node: Leaf(old), new_node: Leaf(new.clone()) });
+    let mut new = old.clone();
+    let options = self.grammar.lexer.fix(&*old, &self.tense);
+    if !options.is_empty() {
+      new = options[self.rng.gen::<usize>() % options.len()].clone();
+      assert!(self.tense.union_checked(&new.tenses).is_empty());
+    }
+    let (old_matches, new_matches) = (vec![old.clone()], vec![new.clone()]);
+    self.diff.push(Diff::Wrong(Wrong { errors, old_matches, new_matches }));
     new
   }
 
@@ -88,58 +90,35 @@ impl<'a, 'b, T: Payload> State<'a, 'b, T> {
 
     // Correct tense errors in each of the tree's children.
     let Derivation { children, rule, value } = new.borrow();
+    let mut diff = vec![];
     let mut checked = vec![false; rule.rhs.len()];
     let mut children = children.clone();
+    let mut child_diffs: Vec<_> = rule.rhs.iter().map(|_| vec![]).collect();
+    std::mem::swap(&mut diff, &mut self.diff);
     for i in rule.precedence.iter().cloned() {
       checked[i] = true;
       children[i] = self.recurse(children[i].clone());
+      std::mem::swap(&mut child_diffs[i], &mut self.diff);
     }
     let mut tense = Tense::default();
     std::mem::swap(&mut tense, &mut self.tense);
     for (i, _) in checked.into_iter().enumerate().filter(|x| !x.1) {
       children[i] = self.recurse(children[i].clone());
+      std::mem::swap(&mut child_diffs[i], &mut self.diff);
       self.tense = Tense::default();
     }
-    std::mem::swap(&mut tense, &mut self.tense);
 
+    // Restore our original state and compute a diff.
     let new = Rc::new(Derivation { children, rule, value: value.clone() });
-    self.mapping.push(Mapping { errors, old_node: Node(old), new_node: Node(new.clone()) });
-    new
-  }
-}
-
-// A helper used to compute a diff between an input tree and its correction.
-
-fn clone_tree<'a, T: Payload>(tree: &Derivation<'a, T>) -> Derivation<'a, T> {
-  let Derivation { children, rule, value } = tree;
-  Derivation { children: children.clone(), rule, value: value.clone() }
-}
-
-fn compute_diff<T>(mapping: &[Mapping<T>], new: &Child<T>, out: &mut Vec<Diff<T>>) {
-  let f = |x: &&Mapping<T>| match (&x.new_node, new) {
-    (Leaf(a), Leaf(b)) => Rc::ptr_eq(a, b),
-    (Node(a), Node(b)) => Rc::ptr_eq(a, b),
-    _ => false,
-  };
-  let m = mapping.iter().filter(f).next().unwrap();
-  if m.errors.is_empty() {
-    match new {
-      Leaf(x) => out.push(Diff::Right(x.clone())),
-      Node(x) => x.children.iter().for_each(|y| compute_diff(mapping, y, out)),
+    std::mem::swap(&mut diff, &mut self.diff);
+    std::mem::swap(&mut tense, &mut self.tense);
+    if errors.is_empty() {
+      child_diffs.into_iter().for_each(|mut x| self.diff.append(&mut x));
+    } else {
+      let (old_matches, new_matches) = (old.matches(), new.matches());
+      self.diff.push(Diff::Wrong(Wrong { errors, old_matches, new_matches }));
     }
-  } else {
-    let old_matches = get_matches(&m.old_node);
-    let new_matches = get_matches(&m.new_node);
-    let mut errors = m.errors.clone();
-    errors.sort();
-    out.push(Diff::Wrong(Wrong { errors, old_matches, new_matches }));
-  }
-}
-
-fn get_matches<T>(child: &Child<T>) -> Vec<Rc<Match<T>>> {
-  match child {
-    Leaf(x) => vec![x.clone()],
-    Node(x) => x.matches(),
+    new
   }
 }
 
@@ -174,10 +153,8 @@ impl<'a, T: Payload> Corrector<'a, T> {
 
   pub fn correct(&self, rng: &mut Rng, tree: &'a Derivation<'a, T>) -> Correction<'a, T> {
     let Self { generator, grammar } = self;
-    let mut state = State { generator, grammar, mapping: vec![], rng, tense: Tense::default() };
-    let new = state.see_node(Rc::new(clone_tree(tree)));
-    let mut diff = vec![];
-    compute_diff(&state.mapping, &Node(new.clone()), &mut diff);
-    Correction { diff, tree: clone_tree(&new) }
+    let mut state = State { diff: vec![], generator, grammar, rng, tense: Tense::default() };
+    let new = state.see_node(Rc::new(State::clone_tree(tree)));
+    Correction { diff: state.diff, tree: State::clone_tree(&new) }
   }
 }
